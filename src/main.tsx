d78efbd3e7c1c313fb18d9b2aba845857e81ae8d
annaKreimerAyash <anna291289@gmail.com>
2024-12-16 11:01:50 +0200

    BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-browser-router'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter basename="/react-portfolio/">
+    <Routes>
+      <Route path="*" element={<App />}/>
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
